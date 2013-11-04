9e85b4dfc56305e751cf0eed3955b81fa7ebec79
Emil Rehnberg <emil.rehnberg@apoex.se>
2013-11-04 12:40:22 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etodo::Application.config.secret_key_base = '9207bcdc0aee38be1ca14a6fa3a817683e88c13d8487585dca52f5409e6cbd7d12d68a18507fb34df98edc40d83c85083ae93631e1c3e7fe50eea6bbe3a228bc'
